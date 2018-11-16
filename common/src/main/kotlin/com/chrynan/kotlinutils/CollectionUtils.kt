10072e9a10ee4a0b8de1706cbbd8ac9216efc9b7
Chris <ckeenan@starry.com>
2018-11-16 13:12:57 -0500

    Added additional functions

--- common/src/main/kotlin/com/chrynan/kotlinutils/CollectionUtils.kt
+++ common/src/main/kotlin/com/chrynan/kotlinutils/CollectionUtils.kt
@@ -1,2 +1,11 @@
 package com.chrynan.kotlinutils
 
+fun <T> Collection<T>.doForEach(block: (T) -> Unit): Collection<T> {
+    forEach(block)
+    return this
+}
+
+fun <T> Collection<T>.doForEachIndexed(block: (Int, T) -> Unit): Collection<T> {
+    forEachIndexed(block)
+    return this
+}
--- common/src/main/kotlin/com/chrynan/kotlinutils/StandardUtils.kt
+++ common/src/main/kotlin/com/chrynan/kotlinutils/StandardUtils.kt
@@ -3,3 +3,9 @@
 package com.chrynan.kotlinutils
 
 inline fun <T> T?.perform(block: T.() -> Unit): Unit = this?.run(block) ?: Unit
+
+inline fun perform(block: () -> Unit): Unit = block()
+
+inline fun <T> T.thenDo(block: (T) -> Unit): T = also(block)
+
+val DoNothing: () -> Unit = {}
