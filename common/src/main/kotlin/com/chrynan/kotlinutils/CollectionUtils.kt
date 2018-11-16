70f1a0cc25eabdb9bf9c4862114c778f28fa2c2c
Chris <ckeenan@starry.com>
2018-11-16 13:22:37 -0500

    Created mapOfNotNull() function in CollectionUtils

--- common/src/main/kotlin/com/chrynan/kotlinutils/CollectionUtils.kt
+++ common/src/main/kotlin/com/chrynan/kotlinutils/CollectionUtils.kt
@@ -11,3 +11,7 @@ fun <T> Collection<T>.doForEachIndexed(block: (Int, T) -> Unit): Collection<T> {
     forEachIndexed(block)
     return this
 }
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified K, reified V> mapOfNotNull(vararg pairs: Pair<K, V?>): Map<K, V> =
+    pairs.filter { it.second != null }.toMap() as Map<K, V>
